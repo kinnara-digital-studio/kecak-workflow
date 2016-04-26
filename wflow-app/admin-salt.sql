30f0ba0db81b6560d2d4c9aba9d4495ab878d616
Aristo Hadisoeganda <aristo.hadisoeganda@gmail.com>
2016-04-26 18:13:40 +0700

    add create table in salt sql

--- wflow-app/admin-salt.sql
+++ wflow-app/admin-salt.sql
@@ -1,2 +1,3 @@
-INSERT INTO `dir_user_salt` (`id`,`userId`,`randomSalt`) VALUES ('f8b1e5aa-959a-43b7-a15a-8b3a6994b39b','admin','805A1624A5C99E5DEBB6F69089C4E08F');
-UPDATE dir_user set password = 'B6430F5714DF8D4746434879D6708BD6' WHERE id='admin';
+CREATE TABLE dir_user_salt (id varchar(255) PRIMARY KEY, userId varchar(255), randomSalt varchar(255));
+INSERT INTO dir_user_salt (id,userId,randomSalt) VALUES ('f8b1e5aa-959a-43b7-a15a-8b3a6994b39b','admin','805A1624A5C99E5DEBB6F69089C4E08F');
+update  dir_user set password = 'B6430F5714DF8D4746434879D6708BD6' where id='admin';
