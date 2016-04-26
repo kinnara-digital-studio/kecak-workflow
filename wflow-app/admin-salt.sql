ef5d23e84cc235b51c85fbbb5bf8d38a2f4bfb02
priasa <ardi.priasa@gmail.com>
2016-04-26 16:37:37 +0700

    user admin salt

---
+++ wflow-app/admin-salt.sql
@@ -0,0 +1,2 @@
+INSERT INTO `dir_user_salt` (`id`,`userId`,`randomSalt`) VALUES ('f8b1e5aa-959a-43b7-a15a-8b3a6994b39b','admin','805A1624A5C99E5DEBB6F69089C4E08F');
+UPDATE dir_user set password = 'B6430F5714DF8D4746434879D6708BD6' WHERE id='admin';
