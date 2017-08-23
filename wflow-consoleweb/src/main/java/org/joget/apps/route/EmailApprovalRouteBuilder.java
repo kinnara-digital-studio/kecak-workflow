7e61a8800e49bfe1f30934fbd11a9d5fb58ea97d
aristo_sh <aristo_sh>
2017-08-23 17:26:29 +0700

    fix error buildNumber, fix bugs error when loading empty email approval settings

--- wflow-consoleweb/src/main/java/org/joget/apps/route/EmailApprovalRouteBuilder.java
+++ wflow-consoleweb/src/main/java/org/joget/apps/route/EmailApprovalRouteBuilder.java
@@ -20,20 +20,36 @@ public class EmailApprovalRouteBuilder extends RouteBuilder {
 		if (emailFolder == null) {
 			emailFolder = "INBOX";
 		}
-		
-		if (emailAccount != null && emailPassword != null && emailProtocol != null && emailHost != null
-				&& emailPort != null) {
+
+		// set default port
+		if(emailPort == null || emailPort.isEmpty()) {
+			if("imap".equals(emailProtocol))
+				emailPort = "143"; // default IMAP
+			else if("imaps".equals(emailProtocol))
+				emailPort = "993"; // default IMAPS
+		}
+
+		// set default folder
+		if(emailFolder == null || emailFolder.isEmpty())
+			emailFolder = "INBOX";
+
+		if (emailAccount != null && !emailAccount.isEmpty() && emailPassword != null && !emailPassword.isEmpty()
+				&& emailProtocol != null && !emailProtocol.isEmpty() && emailHost != null && !emailHost.isEmpty()
+				&& emailPort != null && !emailPort.isEmpty()) {
+
 			StringBuilder fromUriBuilder = new StringBuilder();
 			fromUriBuilder.append(emailProtocol).append("://").append(emailHost).append(":").append(emailPort);
 			fromUriBuilder.append("?username=").append(emailAccount);
 			fromUriBuilder.append("&password=").append(emailPassword);
-			fromUriBuilder.append("&folderName=").append(emailFolder.isEmpty() ? "INBOX" : emailFolder);
+			fromUriBuilder.append("&folderName=").append(emailFolder);
 			fromUriBuilder.append("&delete=false&unseen=true");
 
 			String fromUri = fromUriBuilder.toString();
 			LOGGER.info("###fromUri#" + fromUri);
 
 			from(fromUri).beanRef("emailApprovalProcessor", "parseEmail");
+		} else {
+			LOGGER.info("Skipping Email Approval; not configured");
 		}
 	}
 
