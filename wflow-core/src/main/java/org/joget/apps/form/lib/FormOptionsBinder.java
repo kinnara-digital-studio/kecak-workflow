2efa972bd802a8e515a926e170881b3171973afb
aristo_sh <aristo.hadisoeganda@gmail.com>
2018-07-18 16:21:10 +0700

    fix FormOptionsBinder error when dispaying id in label

--- wflow-core/src/main/java/org/joget/apps/form/lib/FormOptionsBinder.java
+++ wflow-core/src/main/java/org/joget/apps/form/lib/FormOptionsBinder.java
@@ -162,7 +162,7 @@ public class FormOptionsBinder extends FormBinder implements FormLoadOptionsBind
                         if (id != null && !id.isEmpty() && label != null && !label.isEmpty()) {
                             newRow.setProperty(FormUtil.PROPERTY_VALUE, id);
                             newRow.setProperty(FormUtil.PROPERTY_LABEL,
-                                    "true".equals("showIdInLabel")
+                                    "true".equals(getPropertyString("showIdInLabel"))
                                             ? String.format("%s (%s)", label, id)
                                             : label);
                             newRow.setProperty(FormUtil.PROPERTY_GROUPING, grouping);
