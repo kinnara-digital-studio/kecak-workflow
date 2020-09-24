75cc3bf43baf24863125771ed1793dbc26d42d23
aristosh <aristo.hadisoeganda@gmail.com>
2020-09-24 13:28:03 +0700

    add StreamHelper, rearrange codes, fix exception in WOrkflowManagerImpl

--- wflow-consoleweb/src/main/java/org/kecak/webapi/json/controller/AuthenticationJsonController.java
+++ wflow-consoleweb/src/main/java/org/kecak/webapi/json/controller/AuthenticationJsonController.java
@@ -5,12 +5,11 @@ import org.joget.apps.app.service.AuthTokenService;
 import org.joget.commons.util.LogUtil;
 import org.joget.directory.model.User;
 import org.joget.directory.model.service.DirectoryManager;
-import org.joget.workflow.model.service.WorkflowUserManager;
 import org.joget.workflow.util.WorkflowUtil;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.kecak.utils.Declutter;
+import org.kecak.commons.util.StreamHelper;
 import org.kecak.webapi.exception.ApiException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -20,17 +19,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.annotation.Nonnull;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Controller
-public class AuthenticationJsonController implements Declutter {
+public class AuthenticationJsonController implements StreamHelper {
     private final String loginHeader = "Authorization";
     private final String refreshHeader = "REF_TOKEN";
     public final static String NEW_TOKEN = "NEW_TOKEN";
@@ -43,34 +44,29 @@ public class AuthenticationJsonController implements Declutter {
     @Autowired
     AuthTokenService authTokenService;
 
-    @Autowired
-    WorkflowUserManager workflowUserManager;
-
     @RequestMapping(value = "/json/authentication/login", method = RequestMethod.POST)
     public void postBasicAuthentication(final HttpServletRequest request,
                                         final HttpServletResponse response) throws IOException {
-
-        LogUtil.info(getClass().getName(), "Executing JSON Rest API [" + request.getRequestURI() + "] in method [" + request.getMethod() + "] as [" + WorkflowUtil.getCurrentUsername() + "]");
+        final JSONObject jsonResponse = new JSONObject();
+        String header = request.getHeader(loginHeader);
 
         try {
-            String username = WorkflowUtil.getCurrentUsername();
-
-            boolean isAdmin = WorkflowUtil.isCurrentUserInRole(WorkflowUtil.ROLE_ADMIN);
-            String loginAs = getOptionalParameter(request, "loginAs", "");
-            User userAs = Optional.of(loginAs)
-                    .map(directoryManager::getUserById)
-                    .orElse(null);
-
-            if (isAdmin && isNotEmpty(loginAs)) {
-                if (userAs == null || isEmpty(userAs.getUsername())) {
-                    throw new ApiException(HttpServletResponse.SC_BAD_REQUEST, "Error logging in as [" + loginAs + "]");
-                }
-
-                LogUtil.info(getClass().getName(), "[" + username + "] is logging in as [" + loginAs + "]");
-
-                workflowUserManager.setCurrentThreadUser(loginAs);
-                username = loginAs;
+            if(header == null) {
+                throw new ApiException(HttpServletResponse.SC_BAD_REQUEST, "Invalid request header");
             }
+
+            if(!header.startsWith("Basic ")) {
+                throw new ApiException(HttpServletResponse.SC_BAD_REQUEST, "Only receive Basic Authentication");
+            }
+            String[] tokens = extractAndDecodeHeader(header, request);
+
+            String username = tokens[0];
+            LogUtil.debug(this.getClass().getName(), "Basic authentication found for user " + username);
+            String password = tokens[1];
+
+            boolean invalidLogin = !directoryManager.authenticate(username, password);
+            if (invalidLogin)
+                throw new ApiException(HttpServletResponse.SC_UNAUTHORIZED, "Invalid Username or Password");
 
             JSONObject requestPayload;
             try {
@@ -82,20 +78,18 @@ public class AuthenticationJsonController implements Declutter {
             Map<String, Object> claim = parseClaimFromRequestPayload(requestPayload);
             String jwtToken = authTokenService.generateToken(username, claim);
 
-            try {
-                final JSONObject jsonResponse = new JSONObject();
-                jsonResponse.put("status", HttpServletResponse.SC_OK);
-                jsonResponse.put("message", MESSAGE_SUCCESS);
-                jsonResponse.put("token", jwtToken);
+            jsonResponse.put("status", HttpServletResponse.SC_OK);
+            jsonResponse.put("message", MESSAGE_SUCCESS);
+            jsonResponse.put("token", jwtToken);
 
-                response.setContentType("application/json");
-                response.getWriter().write(jsonResponse.toString());
-            } catch (JSONException e) {
-                LogUtil.error(getClass().getName(), e, e.getMessage());
-            }
+            response.setContentType("application/json");
+            response.getWriter().write(jsonResponse.toString());
         } catch (ApiException e) {
+            LogUtil.warn(getClass().getName(), "Error [" + e.getErrorCode() + "] message [" + e.getMessage() + "]");
+            response.sendError(e.getErrorCode(), "Error " + e.getErrorCode() + " : " + e.getMessage());
+        } catch (Exception e) {
             LogUtil.error(getClass().getName(), e, e.getMessage());
-            response.sendError(e.getErrorCode(), e.getMessage());
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal Server Error");
         }
     }
 
@@ -156,8 +150,7 @@ public class AuthenticationJsonController implements Declutter {
     @RequestMapping(value = "json/authentication/refresh", method = RequestMethod.POST)
     public void refreshToken(final HttpServletRequest request,
                              final HttpServletResponse response) throws IOException {
-
-        LogUtil.info(getClass().getName(), "Executing JSON Rest API [" + request.getRequestURI() + "] in method [" + request.getMethod() + "] as [" + WorkflowUtil.getCurrentUsername() + "]");
+        final JSONObject jsonResponse = new JSONObject();
 
         String header = request.getHeader(loginHeader);
         String refToken = request.getHeader(refreshHeader);
@@ -168,8 +161,6 @@ public class AuthenticationJsonController implements Declutter {
             try {
                 String newToken = authTokenService.refreshToken(token, refToken);
                 response.setHeader(NEW_TOKEN, newToken);
-
-                final JSONObject jsonResponse = new JSONObject();
                 jsonResponse.put("status", HttpServletResponse.SC_OK);
                 jsonResponse.put("message", MESSAGE_SUCCESS);
                 response.getWriter().write(jsonResponse.toString());
@@ -302,21 +293,6 @@ public class AuthenticationJsonController implements Declutter {
                         } catch (JSONException ignored) { }
                     }
                 });
-    }
-
-    /**
-     * Get optional parameter
-     *
-     * @param request
-     * @param parameterName
-     * @param defaultValue
-     * @return
-     */
-    private String getOptionalParameter(@Nonnull HttpServletRequest request, @Nonnull String parameterName, String defaultValue) {
-        return Optional.of(parameterName)
-                .map(request::getParameter)
-                .filter(not(String::isEmpty))
-                .orElse(defaultValue);
     }
 //
 //
