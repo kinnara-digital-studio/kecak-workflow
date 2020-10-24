14484c86f7bb1cd059f70d3574619568f9662fa7
aristosh <aristo.hadisoeganda@gmail.com>
2020-10-24 11:04:02 +0700

    restructure Bootstrap theme
    (cherry picked from commit e14239be17a280a1e6ba4e67127fc7a41f3abaf1)

--- wflow-core/src/main/java/org/kecak/apps/userview/model/BootstrapUserviewTheme.java
+++ wflow-core/src/main/java/org/kecak/apps/userview/model/BootstrapUserviewTheme.java
@@ -10,8 +10,28 @@ import java.util.Map;
  *
  * Bootstrap Web UI Framework for Userview Theme
  */
-@Deprecated
 public interface BootstrapUserview {
+    /**
+     * WARNING!!!!! these methods have to be implemented in Kecak Core
+     * JSP File in Plugins cannot be recognized by Kecak Core
+     * @return
+     */
+
+    String getUserviewJsp();
+
+    String getPreviewJsp();
+
+    String getDataListJsp();
+
+    String getFormJsp();
+
+    String getRunProcessJsp();
+
+    String getLoginJsp();
+
+    default String getUnauthorizedJsp() {
+        return "userview/plugin/unauthorized.jsp";
+    }
 
     /**
      * Render core element
